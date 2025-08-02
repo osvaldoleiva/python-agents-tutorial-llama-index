cb59b4562422e1496b0bda0899648b0445bee762
Osvaldo Leiva <osvaldoleivabarria1@gmail.com>
2025-08-02 05:01:41 -0400

    basic agent using openai as llm

---
+++ 1_basic_agent.py
@@ -0,0 +1,36 @@
+from llama_index.llms.openai import OpenAI
+from llama_index.core.agent.workflow import FunctionAgent
+
+def multiply(a: float, b: float) -> float:
+    """Multiply two numbers and returns the product"""
+    return a * b
+
+
+def add(a: float, b: float) -> float:
+    """Add two numbers and returns the sum"""
+    return a + b
+
+# Setup llm to use as agent
+llm = OpenAI(model="gpt-4o-mini")
+
+# Initialize agent requirements:
+# set of tools in this case the functions create above
+# a llm
+# a system prompt
+
+workflow = FunctionAgent(
+    tools=[multiply, add],
+    llm=llm,
+    system_prompt="You are an agent that can perform basic mathematical operations using tools."
+)
+
+
+async def main():
+    response = await workflow.run(user_msg="What is 20+(2*4)?")
+    print(response)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
